5f8bb65eb7df61b2130630d73ff59589320c3488
alejandro gaytan garcia <gaytano9@live.com.mx>
2019-11-14 11:51:54 -0600

    Agregar archivos de proyecto.

---
+++ delegado/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace delegado
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Form2 frm =new Form2();
+            frm.pasado += Frm_pasado;
+            frm.Show();
+        }
+
+        private void Frm_pasado(string texto)
+        {
+            textBox1.Text += texto + Environment.NewLine;
+        }
+
+        private void agregarTexto(string texto) {
+            textBox1.Text = Text + Environment.NewLine;
+        }
+    }
+}
---
+++ delegado/Form2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace delegado
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        public delegate void pasar(string texto);
+        public event pasar pasado;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pasado(textBox1.Text);
+            textBox1.Clear();
+        }
+    }
+}
